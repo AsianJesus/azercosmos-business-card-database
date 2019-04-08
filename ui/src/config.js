6ab3e0616d0f16b977c5d486069e305d7f8e5cc5
Seymur Omarov <omarov.seymur@outlook.com>
2019-04-08 15:17:42 +0400

    fix url for production

--- ui/src/config.js
+++ ui/src/config.js
@@ -1,2 +1,2 @@
-export const serverURL = 'http://localhost/azercosmos-business-card-database/api/public/'
-// export const serverURL = 'https://erptest.azercosmos.az/azercosmos-business-card-database/api/public/'
+// export const serverURL = 'http://localhost/azercosmos-business-card-database/api/public/'
+export const serverURL = 'https://erptest.azercosmos.az/azercosmos-business-card-database/api/public/'
