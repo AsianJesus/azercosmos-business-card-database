330e6807890ff057871c0c5a0f5ed48a5c1b7c3a
Seymur Omarov <omarov.seymur@outlook.com>
2019-04-11 10:57:26 +0400

    fix url

--- ui/src/config.js
+++ ui/src/config.js
@@ -1,3 +1,3 @@
 const getUrl = window.location;
 let baseUrl = getUrl.protocol + '//' + getUrl.hostname
-export const serverURL = baseUrl + '/azercosmos-business-card-database/api/public'
+export const serverURL = baseUrl + '/azercosmos-business-card-database/api/public/'
