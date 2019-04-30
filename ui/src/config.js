02d0da92694b9dc19d479f5f85f59a940a7261dd
fruiteatearth@gmail.com <fruiteatearth@gmail.com>
2019-04-30 22:18:20 +0400

    Initialized android app

--- ui/src/config.js
+++ ui/src/config.js
@@ -1,3 +1,4 @@
 const getUrl = window.location;
-let baseUrl = getUrl.protocol + '//' + getUrl.hostname
+// let baseUrl = getUrl.protocol + '//' + getUrl.hostname
+let baseUrl = 'http://localhost'
 export const serverURL = baseUrl + '/azercosmos-business-card-database/api/public/'
