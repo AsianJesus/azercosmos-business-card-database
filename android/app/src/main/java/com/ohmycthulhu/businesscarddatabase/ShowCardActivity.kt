79977e8bc409dce472484ba4b498c28faf239045
fruiteatearth@gmail.com <fruiteatearth@gmail.com>
2019-05-04 22:48:35 +0400

    Delete card ability for Android

--- android/app/src/main/java/com/ohmycthulhu/businesscarddatabase/ShowCardActivity.kt
+++ android/app/src/main/java/com/ohmycthulhu/businesscarddatabase/ShowCardActivity.kt
@@ -1,22 +1,33 @@
 package com.ohmycthulhu.businesscarddatabase
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.app.Dialog
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.drawable.Drawable
 import android.os.AsyncTask
 import android.os.Bundle
+import android.support.v4.app.DialogFragment
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.request.StringRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_show_card.*
 import java.io.InputStream
 import java.net.URL
 
 class ShowCardActivity : AppCompatActivity() {
     private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var card: BusinessCard
+    private var requestQueue: RequestQueue? = null
     private val REQUEST_EDIT_CARD  = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +46,7 @@ class ShowCardActivity : AppCompatActivity() {
     }
 
     private fun setFields (card: BusinessCard) {
+        this.card = card
         showCardName.text = card.name
         showCardCompany.text = card.company ?: ""
         showCardPosition.text = card.position ?: ""
@@ -50,7 +62,7 @@ class ShowCardActivity : AppCompatActivity() {
             LoadImage(showCardImage, this).execute(url)
         }
         if (card.isMine) {
-            showCardEditButton.visibility = View.VISIBLE
+            showCardButtons.visibility = View.VISIBLE
 
             showCardEditButton.setOnClickListener {
                 Intent(this, EditCardActivity::class.java).also { intent ->
@@ -58,7 +70,32 @@ class ShowCardActivity : AppCompatActivity() {
                     startActivityForResult(intent, REQUEST_EDIT_CARD)
                 }
             }
+
+            showCardDeleteButton.setOnClickListener {
+                val dialog = DeleteCardDialog()
+                dialog.setActivity(this)
+                dialog.show(supportFragmentManager, "delete_card")
+            }
         }
+    }
+
+    fun deleteCard () {
+        Toast.makeText(this, "Deleting card #${card.id}", Toast.LENGTH_SHORT).show()
+        val url = "${sharedPreferences.getString("api_address", "http://192.168.1.8")}/business-cards/${card.id}"
+        val request = StringRequest(Request.Method.DELETE, url, {
+            Toast.makeText(this, "We deleted card #${card.id}!", Toast.LENGTH_SHORT).show()
+            setResult(Activity.RESULT_OK)
+            finish()
+        }, {
+            Toast.makeText(this, "Couldn't delete the card", Toast.LENGTH_SHORT).show()
+            Log.e("delete error", it.message)
+        })
+
+        request.tag = "delete_card"
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(this)
+        }
+        requestQueue?.add(request)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -71,6 +108,7 @@ class ShowCardActivity : AppCompatActivity() {
         }
     }
 }
+
 private class LoadImage() : AsyncTask<String, Void, Drawable>() {
     private lateinit var imageView: ImageView
     private lateinit var mContext: Context
@@ -99,3 +137,25 @@ private class LoadImage() : AsyncTask<String, Void, Drawable>() {
         }
     }
 }
+
+class DeleteCardDialog : DialogFragment() {
+    private var showCardActivity: ShowCardActivity? = null
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+            builder.setMessage("Delete card? You cannot undo this action!")
+                .setPositiveButton("Delete anyway!") { dialog, which ->
+                    showCardActivity?.deleteCard()
+                }.setNegativeButton("Cancel") { dialog, which ->
+
+                }
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+
+    fun setActivity (showCardActivity: ShowCardActivity) {
+        this.showCardActivity = showCardActivity
+    }
+
+}
+
